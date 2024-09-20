90a47e3df8f516adc7e13db0f6de88a96a1014a8
Dominik Ryczek <dominik.ryczek@expereo.com>
2024-09-20 16:27:49 +0200

    LWC for Prompt Playground

---
+++ src/main/lwc/promptBusinessPlayground/promptBusinessPlayground.js
@@ -0,0 +1,58 @@
+import {api, LightningElement, track, wire} from 'lwc';
+import { getRecord } from "lightning/uiRecordApi";
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+import getModelResponse from '@salesforce/apex/PromptBusinessPlaygroundController.getModelResponse';
+
+
+export default class PromptBusinessPlayground extends LightningElement {
+    caseId;
+    @track userPrompt;
+    contextType;
+    contextOptions;
+    modelResponse;
+    showSpinner = false;
+
+
+    connectedCallback() {
+        this.contextType = 'Full';
+        this.contextOptions = [
+            { label: 'Case with all Emails', value: 'Full' },
+            { label: 'Case with today\'s Emails', value: 'Today' }
+        ];
+    }
+
+    handleCaseIdChange(event) {
+        this.caseId = event.target.value;
+    }
+
+    handleUserPromptChange(event) {
+        this.userPrompt = event.target.value;
+    }
+
+    handleContextChange(event) {
+        this.contextType = event.detail.value;
+    }
+
+    async getResponse() {
+        this.showSpinner = true;
+        try {
+            this.modelResponse = await getModelResponse(
+                {caseId: this.caseId, contextType: this.contextType, userPrompt: this.userPrompt});
+        } catch (error) {
+            this.showSpinner = false;
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error',
+                    message: error.body.message,
+                    variant: 'error'
+                })
+            );
+        }
+        this.showSpinner = false;
+    }
+
+    get disableButton() {
+        return !this.userPrompt || !this.caseId;
+    }
+}
